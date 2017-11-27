c862d4dac3c1b9b26a5090db0e27733b6e9b6dc1
Rahisul Haque <rahisul@icloud.com>
2017-11-27 10:30:13 -0800

    updates

--- charpter8/List00.py
+++ charpter8/List00.py
@@ -1,2 +1,23 @@
+##############################################################################################
+#                                                                                            #
+#                          CS 87 A, Python Programming                                       #
+#                      This code is written by Rahisul Haque                                 #
+#                           e-mail: rahisul@icloud.com                                       #
+#                                                                                            #
+#                                                                                            #
+#                                                                                            #
+##############################################################################################
+
+
+#list method
+print("There is couple of built in method. ")
+print("1. comp :")
+print("2. Concatenation: ")
+print("3.  ")
+print("1. comp ")
+
+
 myList = [1, 2, 3]
 print sum(myList);
+myList.append(9)
+print myList
